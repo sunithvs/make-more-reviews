9df0d851efcbd3cfab5f4408dc8e816be27a4a98
sunithvs <sunithvs2002@gmail.com>
2025-03-23 22:49:32 +0530

    feat: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Supabase Auth + Next.js Demo",
+  title: "Make More reviews",
 };
 
 export default function RootLayout({
